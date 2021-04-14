def15debcda6d4134ecaeffc5f5ac85d232f7102
Gino Villalpando <gino@rs21.io>
2021-04-13 23:05:36 -0600

    ran lint

--- src/commands/activity.ts
+++ src/commands/activity.ts
@@ -7,7 +7,7 @@ createCommand({
     try {
       const user = cache.members.get(message.author.id);
       const presence = cache.presences.get("104184695750680576");
-      return message.send(`member is playing ${presence}`);
+      return await message.send(`member is playing ${presence}`);
     } catch (error) {
       return message.send(`Error: ${error}`);
     }
