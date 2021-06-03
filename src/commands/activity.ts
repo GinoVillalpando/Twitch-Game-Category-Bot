d324a8d15b891421b1f20f873292598933e7a682
Gino Villalpando <gino@rs21.io>
2021-06-03 01:34:18 -0600

    connected twitch bot to activity command

--- src/commands/activity.ts
+++ src/commands/activity.ts
@@ -1,5 +1,8 @@
 import { createCommand } from "../utils/helpers.ts";
 import { cache } from "../../deps.ts";
+import { TwitchChat, Channel } from "https://deno.land/x/tmi/mod.ts";
+import { delay } from "https://deno.land/std@0.64.0/async/delay.ts";
+import { configs } from "../../configs.ts";
 
 createCommand({
   name: `activity`,
@@ -7,6 +10,16 @@ createCommand({
     try {
       const user = cache.members.get(message.author.id);
       const presence = cache.presences.get(user.id);
+      const twitchChat = new TwitchChat(configs.oauthToken, "grievxus");
+      await twitchChat.connect();
+      const channel = twitchChat.joinChannel("grievxus");
+
+      const updateTitle = async (channel: Channel, game: string) => {
+        channel.send(`!game ${game}`);
+      };
+
+      updateTitle(channel, presence.activities[0]["name"]);
+
       console.log(presence.activities[0]["name"]);
       return await message.send(
         `member is playing ${presence.activities[0]["name"]}`
